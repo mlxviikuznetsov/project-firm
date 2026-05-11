cf45bae25929b79b8208a4f55ac5045bedb504e5
Maximilian Kuzniatsou <maximiliankuzniatsou@gmail.com>
2026-05-12 01:25:58 +0300

    feat(data): define TEmployee and TWork record types

--- DataTypes.pas
+++ DataTypes.pas
@@ -1,7 +1,32 @@
 unit DataTypes;
 
 interface
-
+uses SysUtils;
+type
+  TEmployee = record
+    Code      : Integer;
+    FullName  : string[100];
+    Position  : string[60];
+    WorkHours : Integer;
+    BossCode  : Integer;
+  end;
+  TWork = record
+    ProjectName  : string[100];
+    Task         : string[200];
+    ExecutorCode : Integer;
+    BossCode     : Integer;
+    IssueDate    : TDateTime;
+    Deadline     : TDateTime;
+  end;
+  PEmployeeNode = ^TEmployeeNode;
+  TEmployeeNode = record
+    Data: TEmployee;
+    Next: PEmployeeNode;
+  end;
+  PWorkNode = ^TWorkNode;
+  TWorkNode = record
+    Data: TWork;
+    Next: PWorkNode;
+  end;
 implementation
-
 end.
