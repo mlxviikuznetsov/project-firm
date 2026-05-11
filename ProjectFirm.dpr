5cd288809c411b189b107b8124bb76e6672ef231
Maximilian Kuzniatsou <maximiliankuzniatsou@gmail.com>
2026-05-12 02:19:25 +0300

    feat: wire up main loop and entry point

--- ProjectFirm.dpr
+++ ProjectFirm.dpr
@@ -13,11 +13,37 @@ uses
   EmployeeList in 'EmployeeList.pas',
   WorkList in 'WorkList.pas';
 
+var
+  Choice: string;
+
 begin
-  try
-    { TODO -oUser -cConsole Main : Insert code here }
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
+  SetConsoleCP866;
+
+  { Главный цикл программы }
+  repeat
+    PrintHeader;
+    PrintMainMenu;
+
+    Choice := PromptStr('Введите номер пункта');
+    WriteLn;
+
+    if Choice = '1' then DoReadFromFile
+    else if Choice = '2' then DoView
+    else if Choice = '3' then DoSort
+    else if Choice = '4' then DoSearch
+    else if Choice = '5' then DoAdd
+    else if Choice = '6' then DoDelete
+    else if Choice = '7' then DoEdit
+    else if Choice = '8' then DoReports
+    else if Choice = '9' then DoExitNoSave
+    else if Choice = '10' then DoExitSave
+    else
+    begin
+      SetColor(CLR_ERROR);
+      WriteLn('  Неверный выбор. Введите число от 1 до 10.');
+      ResetColor;
+      PressEnter;
+    end;
+
+  until False; // Выход через Halt в DoExitNoSave/DoExitSave
 end.
